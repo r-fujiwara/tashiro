15cfe1a8bac5d5b87d52985a9bdc98a2a92b569c
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-10-01 07:33:03 +0900

    quit oshare meta programming

--- lib/tashiro/toritsu_univ.rb
+++ lib/tashiro/toritsu_univ.rb
@@ -1,19 +1,17 @@
 require "logger"
 require "yaml"
 require "pathname"
+require "fssm"
 
 class Tashiro::ToritsuUniv
   class << self
     def execute!(f)
-      config = YAML.load(Pathname.new(f).expand_path.to_s)
+      config = YAML::load_file(Pathname.new(f).expand_path.to_s)
       config["path"].each do |dir|
-        module_eval do <<-EOS
-            FSSM.monitor("#{dir}", "**/*.go") do
-              update do |base, relative|
-                puts "3"
-              end
-            end
-          EOS
+        FSSM.monitor("#{dir}", "**/*.go") do
+          update do |base, relative|
+            puts "3"
+          end
         end
       end
     end
