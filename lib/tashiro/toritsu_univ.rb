1ec1a2ccf6e23b10f5cdf491d6e7ea2556c51d67
r-fujiwara <r-fujiwara@nekojarashi.com>
2015-10-02 09:38:28 +0900

    update

--- lib/tashiro/toritsu_univ.rb
+++ lib/tashiro/toritsu_univ.rb
@@ -8,8 +8,10 @@ class Tashiro::ToritsuUniv
     def execute!(f)
       config = YAML::load_file(Pathname.new(f).expand_path.to_s)
       config["path"].each do |dir|
-        FSSM.monitor("#{dir}", "**/*.go") do
-          update do |base, relative|
+        path = Pathname.new(dir).expand_path.to_s
+        puts "watching....#{path}"
+        FSSM.monitor("#{path}", "*.go") do
+          create do |base, relative|
             puts "3"
           end
         end
